7e2331df10d013dadafd7af2cbe9670ce100eaad
Mesay-AK <mesabe77@gmail.com>
2025-03-21 19:54:23 +0300

    Updated

--- docker-lecture/main.py
+++ docker-lecture/main.py
@@ -1,4 +1,6 @@
 def fizbuzz(n):
+    # FizzBuzz function that prints numbers from 1 to n
+    # If the number is divisible by 3, print 'Fizz' 
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
             print('FizzBuzz')
